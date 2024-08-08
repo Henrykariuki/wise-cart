c690882717edf34f780f649f23b014b0bc9bcad5
Henry Kariuki <henrykariuki17@gmail.com>
2024-08-09 00:30:49 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MyHeader from './components/MyHeader.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <MyHeader />
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { User } from 'lucide-vue-next';
+</script>
+
+<template>
+  <div class="text-3xl font-bold underline">
+    My Header
+    <User/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
